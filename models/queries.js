0e49ea40df002f7746aa06b26a52f4cdc217a1c6
ZManak <javifuer@gmail.com>
2023-01-24 16:24:46 +0100

    getOK

--- models/queries.js
+++ models/queries.js
@@ -1,6 +1,6 @@
 const queries = {
     getEntries: `
-    SELECT e.title, e.content, e.date, e.category, 
+    SELECT e.title, e.content, e.dated, e.category, 
     a.name,a.surname,a.image  
     FROM entries AS e
     INNER JOIN authors AS a
